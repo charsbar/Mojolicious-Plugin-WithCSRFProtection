1b1cf2cd0806785f1b192c2e6baf122466a4de1e
Kenichi Ishigaki <ishigaki@cpan.org>
2025-05-02 16:06:49 +0900

    Checking in changes prior to tagging of version 1.02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 7105fff..15eda80 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     {{$NEXT}}
     
    +1.02 2025-05-02T07:06:44Z
    +  - Skip rendering bad_csrf if mojo is finished
    +
     1.01 2024-04-07T13:58:46Z
     
       - Apply fixes for Mojolicious 9+

--- lib/Mojolicious/Plugin/WithCSRFProtection.pm
+++ lib/Mojolicious/Plugin/WithCSRFProtection.pm
@@ -4,7 +4,7 @@ package Mojolicious::Plugin::WithCSRFProtection;
 
 use Mojo::Base 'Mojolicious::Plugin';
 
-our $VERSION = '1.01';
+our $VERSION = '1.02';
 
 sub register {
     my ( $self, $app ) = @_;
